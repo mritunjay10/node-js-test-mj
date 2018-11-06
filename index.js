16679e9f80ee279ce3c5de7374b9cd103229e2bc
Your NameMritunjay <mjdemon211@gmail.com>
2018-11-06 19:10:22 +0530

    Add cool face API

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app =express()
+  
+app.get('/', (req, res) => res.send('Hello World!'))
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
